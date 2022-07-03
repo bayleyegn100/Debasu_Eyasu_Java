6a53696e6a8560da7c9a10ddcd130d112208eb16
Debasu Eyasu <bayleyegn100@gmail.com>
2022-07-02 17:30:48 -0700

    A body for attackAnotherCharacter for a Warrior class is created

--- RPGInheritance/src/main/java/com/company/Warrior.java
+++ RPGInheritance/src/main/java/com/company/Warrior.java
@@ -3,6 +3,20 @@ package com.company;
 public class Warrior extends Character {
     private int shieldStrength;
 
+    public void attackAnotherCharacter(Constabl constable, Farmer farmer) {
+        if(constable.getAttackPower() > this.getAttackPower() || farmer.getAttackPower() > this.getAttackPower()){
+            System.out.println(this.name + " To over come our enemies we should have to increase our shield strength, speed and stamina.");
+            shieldStrength++;
+            stamina++;
+            speed--;
+        } else{
+            System.out.println("to bit" + constable.getName() + " and " + farmer.getName() + " we should increase our speed, strength and attack power.");
+            strength++;
+            attackPower++;
+            speed++;
+        }
+    }
+
     public int getShieldStrength() {
         return shieldStrength;
     }
