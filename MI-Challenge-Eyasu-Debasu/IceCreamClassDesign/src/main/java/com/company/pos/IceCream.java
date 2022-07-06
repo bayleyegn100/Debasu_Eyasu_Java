f68a79453a1338951fda10dbae78fa7e96b8b530
Debasu Eyasu <bayleyegn100@gmail.com>
2022-07-05 17:45:43 -0700

    For sales point of view an ice cream class is created, necessery properties are incorporated, getters, setters and tostrings are included. In additon three methods are created.

--- MI-Challenge-Eyasu-Debasu/IceCreamClassDesign/src/main/java/com/company/pos/IceCream.java
+++ MI-Challenge-Eyasu-Debasu/IceCreamClassDesign/src/main/java/com/company/pos/IceCream.java
@@ -1,2 +1,78 @@
-package com.company.pos;public class IceCream {
+package com.company.pos;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class IceCream {
+    private int temp;
+    private String[] flavor= {"Strawberry", "Chocolate", "Buttered Pecan"};
+    private int customerSatsfaction;
+    private boolean isSupportedByImage;
+
+    public void keepColdTheIceCream(){
+        if(this.temp >= 40){
+            System.out.println("We have to find a set of materials that keeps the ice-cram room temp below 40 degree favorite.");
+            temp--;
+            customerSatsfaction--;
+        } else {
+            System.out.println(this.temp + " is suitable temp to keep the ice-cream cold");
+            customerSatsfaction++;
+        }
+    }
+    public void haveEnoughFlavors(){
+        System.out.println(this.flavor + " are available to the users");
+        System.out.println("Have a look on those posted pictures and come back with your choice.");
+    }
+
+    public void customerSatisfactions(){
+        Scanner myScanner = new Scanner(System.in);
+        System.out.println("We do have " + this.flavor + " flavored cold ice-cream.");
+        System.out.println("If your favorite is not included on the list let us know we will work on it for next time.");
+        String customerDemand = myScanner.nextLine();
+        System.out.println("You have entered " + customerDemand + " flavor. Sorry for not fulfilling your demand. We will get you next time with your favorite.");
+        customerSatsfaction--;
+    }
+
+
+    public int getTemp() {
+        return temp;
+    }
+
+    public void setTemp(int temp) {
+        this.temp = temp;
+    }
+
+    public String[] getFlavor() {
+        return flavor;
+    }
+
+    public void setFlavor(String[] flavor) {
+        this.flavor = flavor;
+    }
+
+    public int getCustomerSatsfaction() {
+        return customerSatsfaction;
+    }
+
+    public void setCustomerSatsfaction(int customerSatsfaction) {
+        this.customerSatsfaction = customerSatsfaction;
+    }
+
+    public boolean isSupportedByImage() {
+        return isSupportedByImage;
+    }
+
+    public void setSupportedByImage(boolean supportedByImage) {
+        isSupportedByImage = supportedByImage;
+    }
+
+    @Override
+    public String toString() {
+        return "IceCream{" +
+                "temp=" + temp +
+                ", flavor=" + Arrays.toString(flavor) +
+                ", customerSatsfaction=" + customerSatsfaction +
+                ", isSupportedByImage=" + isSupportedByImage +
+                '}';
+    }
 }
